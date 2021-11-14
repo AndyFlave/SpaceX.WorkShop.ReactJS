80cb856658241f835bbbe04339e54667c6fdc1df
AndyFlave <35171083+AndyFlave@users.noreply.github.com>
2021-11-14 16:33:00 +0300

    cancel useLaunches fetch on unmount

--- src/components/useLaunches/useLaunches.js
+++ src/components/useLaunches/useLaunches.js
@@ -1,20 +1,24 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import FetchData from '../../service/FetchData';
 
 const fetchData = new FetchData();
 
 const useLaunches = () => {
-	
+
 	const [data, setData] = useState([]);
 
 	useEffect(() => {
+		let canceled = false;
 		fetchData.getLaunches()
-			.then(launches => setData(state => [...launches]))
-	}, [])
+			.then(launches => {
+				if (!canceled) setData(launches);
+			});
+		return () => { canceled = true; };
+	}, []);
 
-	const getLaunch = id => data.find(item => item.id === id)
+	const getLaunch = id => data.find(item => item.id === id);
 
-	return {data, getLaunch}
-}
+	return { data, getLaunch };
+};
 
 export default useLaunches;
